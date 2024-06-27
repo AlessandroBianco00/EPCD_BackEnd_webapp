0746b3d000770de58064827b88f01eda38f1373f
AlessandroBianco00 <alessandrobianco2000@gmail.com>
2024-06-27 22:24:43 +0200

    Aggiunti decoratori required

--- EPCDWebApp/ElencoMagazzinoWebApp/Models/Product.cs
+++ EPCDWebApp/ElencoMagazzinoWebApp/Models/Product.cs
@@ -1,10 +1,15 @@
-﻿namespace ElencoMagazzinoWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ElencoMagazzinoWebApp.Models
 {
     public class Product
     {
+        [Required(ErrorMessage = "Inserire il nome")]
         public string ProductName { get; set; }
         public int ProductId { get; set; }
+        [Required(ErrorMessage = "Inserire descrizione")]
         public string Description { get; set; }
+        [Required, Range(1, 1000)]
         public int QuantityAvailable { get; set; }
     }
 }
